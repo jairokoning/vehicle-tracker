abea95845aa720049c93e377b47873fcb71b53dd
Jairo Koning <jairo.koning@gmail.com>
2024-12-19 05:42:24 -0300

    feat(web): add admin page

---
+++ apps/web/src/app/admin/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { type RefObject, useRef } from "react";
+import { useMap } from "../../hooks/useMap";
+
+export function AdminPage() {
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  //useMap(mapContainerRef);
+  useMap(mapContainerRef as RefObject<HTMLDivElement>);
+
+  return <div className="h-full w-full" ref={mapContainerRef} />;
+}
+
+export default AdminPage;
